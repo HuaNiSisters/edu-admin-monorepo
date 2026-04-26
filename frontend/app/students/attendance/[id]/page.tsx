fb454a8f466f846d76d370518c57ab32d9047cfa
Manhua Lu <manhua.lu@student.unsw.edu.au>
2026-04-26 18:58:04 +1000

    Display class details for attendance

--- frontend/app/students/attendance/[id]/page.tsx
+++ frontend/app/students/attendance/[id]/page.tsx
@@ -5,6 +5,8 @@ import { useParams } from "next/navigation";
 import { useAsync } from "@/hooks/use-async";
 import { classService } from "@/lib/services";
 import { ClassTimeWithSubjectAndTutor } from "@/lib/api/types";
+import { start } from "repl";
+import { formatValuesRemoveUnderscores } from "@/utils/text-utils";
 
 const ViewClassAttendance = () => {
   const params = useParams();
@@ -24,7 +26,25 @@ const ViewClassAttendance = () => {
     run(fetchClassData);
   }, [params.id, run]);
 
-  return <div>{classData?.subject_name}</div>;
+  return (
+    <>
+      <div>
+        <h1 className="text-2xl font-bold text-gray-900">
+          {isPending && !classData
+            ? "Loading…"
+            : classData?.subject_name
+              ? `${classData.subject_name} - Grade ${classData.grade} ${classData.day_of_week} ${classData.start_time} –
+              ${classData.end_time} (${formatValuesRemoveUnderscores(classData?.location ?? "")})`
+              : "Class Attendance"}
+        </h1>
+        {classData && (
+          <p className="mt-1 text-sm text-gray-500 space-x-3">
+            {classData.tutor && <span>{classData.tutor}</span>}
+          </p>
+        )}
+      </div>
+    </>
+  );
 };
 
 export default ViewClassAttendance;
