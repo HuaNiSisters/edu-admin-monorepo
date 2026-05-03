80c193b0d4655e60d56e2028257f086a0f401ad2
Manhua Lu <manhua.lu@student.unsw.edu.au>
2026-05-03 22:09:25 +1000

    Put enrollments of student in dropdowns for per term

--- frontend/app/student/_components/enrolled-classes.tsx
+++ frontend/app/student/_components/enrolled-classes.tsx
@@ -3,7 +3,7 @@
 import { useEffect, useState } from "react";
 import { Button } from "@/components/ui/button";
 import EnrolDialog from "./enrol-dialog";
-import { EnrolmentWithClassAndTerm } from "@/lib/api/types";
+import { EnrolmentWithClassAndTerm, Term } from "@/lib/api/types";
 import { enrolmentService } from "@/lib/services";
 import { useAsync } from "@/hooks/use-async";
 import {
@@ -15,6 +15,12 @@ import {
   TableRow,
 } from "@/components/ui/table";
 import { formatValuesRemoveUnderscores } from "@/utils/text-utils";
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "@/components/ui/collapsible";
+import { ChevronDownIcon } from "lucide-react";
 
 interface EnrolledClassesProps {
   studentId: string;
@@ -34,8 +40,29 @@ export default function EnrolledClasses({ studentId }: EnrolledClassesProps) {
   };
 
   useEffect(() => {
-    run(fetchEnrolments);
+    fetchEnrolments();
   }, [studentId]);
+
+  const getTermsWhereEnrolled = (enrolments: EnrolmentWithClassAndTerm[]) => {
+    const uniqueTermsMap: Record<string, Term> = {};
+    enrolments.forEach((enrolment) => {
+      const term = enrolment.Term;
+      if (!uniqueTermsMap[term.term_id]) {
+        uniqueTermsMap[term.term_id] = term;
+      }
+    });
+    return Object.values(uniqueTermsMap).sort(
+      (a, b) =>
+        new Date(b.start_date).getTime() - new Date(a.start_date).getTime(),
+    );
+  };
+
+  const getEnrolmentsForTerm = (
+    enrolments: EnrolmentWithClassAndTerm[],
+    termId: string,
+  ) => {
+    return enrolments.filter((enrolment) => enrolment.Term.term_id === termId);
+  };
 
   return (
     <div>
@@ -47,13 +74,86 @@ export default function EnrolledClasses({ studentId }: EnrolledClassesProps) {
           run(fetchEnrolments);
         }}
       />
-      <div className="flex gap-5 py-2 justify-between">
-        <span className="text-xl font-bold">Enrolments</span>
-        <Button onClick={() => setIsEnrolDialogOpen(true)}>Enrol</Button>
+      <div className="py-4">
+        <div className="flex justify-between">
+          <span className="text-xl font-bold">Enrolments</span>
+          <Button onClick={() => setIsEnrolDialogOpen(true)}>Enrol</Button>
+        </div>
+        <div>
+          These are the terms where the student has had a least one enrolment.
+        </div>
       </div>
+
       {isPending && <div>Loading enrolments...</div>}
       {!isPending && enrolments.length === 0 && <div>No enrolments found.</div>}
-      {!isPending && enrolments.length > 0 && (
+      {getTermsWhereEnrolled(enrolments).length > 0 && (
+        <div className="mb-4">
+          {getTermsWhereEnrolled(enrolments).map((term) => (
+            <div key={term.term_id} className="mb-2">
+              <Collapsible className="rounded-md data-[state=open]:bg-muted">
+                <CollapsibleTrigger asChild>
+                  <Button
+                    variant="ghost"
+                    className="group w-full text-md bg-muted"
+                  >
+                    {term.year} Term {term.name}
+                    <ChevronDownIcon className="ml-auto group-data-[state=open]:rotate-180" />
+                  </Button>
+                </CollapsibleTrigger>
+                <CollapsibleContent className="flex flex-col p-1">
+                  <div className="rounded-md border p-2 bg-primary-foreground">
+                    <Table>
+                      <TableHeader>
+                        <TableRow>
+                          <TableHead>Term</TableHead>
+                          <TableHead>Day</TableHead>
+                          <TableHead>Grade</TableHead>
+                          <TableHead>Subject</TableHead>
+                          <TableHead>Time</TableHead>
+                          <TableHead>Location</TableHead>
+                        </TableRow>
+                      </TableHeader>
+                      <TableBody>
+                        {getEnrolmentsForTerm(enrolments, term.term_id).map(
+                          (enrolment) => (
+                            <TableRow key={enrolment.enrolment_id}>
+                              <TableCell>
+                                {enrolment.Term.year} Term {enrolment.Term.name}
+                              </TableCell>
+                              <TableCell>
+                                {enrolment.ClassTime.day_of_week}
+                              </TableCell>
+                              <TableCell>
+                                {enrolment.ClassTime.SubjectOffering.grade}
+                              </TableCell>
+                              <TableCell>
+                                {
+                                  enrolment.ClassTime.SubjectOffering
+                                    .subject_name
+                                }
+                              </TableCell>
+                              <TableCell>
+                                {enrolment.ClassTime.start_time} -{" "}
+                                {enrolment.ClassTime.end_time}
+                              </TableCell>
+                              <TableCell>
+                                {formatValuesRemoveUnderscores(
+                                  enrolment.ClassTime.SubjectOffering.location,
+                                )}
+                              </TableCell>
+                            </TableRow>
+                          ),
+                        )}
+                      </TableBody>
+                    </Table>
+                  </div>
+                </CollapsibleContent>
+              </Collapsible>
+            </div>
+          ))}
+        </div>
+      )}
+      {/* {!isPending && enrolments.length > 0 && (
         <div className="rounded-md border p-2 bg-primary-foreground">
           <Table>
             <TableHeader>
@@ -93,7 +193,7 @@ export default function EnrolledClasses({ studentId }: EnrolledClassesProps) {
             </TableBody>
           </Table>
         </div>
-      )}
+      )} */}
     </div>
   );
 }
